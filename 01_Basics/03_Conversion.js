8dfbc387e42684227800cc418903b266c1dc123d
manavsood221 <msmanavsood74@gmail.com>
2024-06-19 13:43:13 +0000

    conversion file

---
+++ 01_Basics/03_Conversion.js
@@ -0,0 +1,25 @@
+let score = "undefined"
+
+console.log(typeof score)
+
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+
+// "33" => 33
+// "33abc" =>NaN
+// "true" => 1; false => 0;
+let isLoggedIn = "manav";
+
+let booleanISLoggedIn = Boolean(isLoggedIn);
+console.log(booleanISLoggedIn);
+
+//1 => True; 0=> false
+// "" => false
+// "Manav" => true
+
+let anyNumb = 33;
+
+let stringNumb = String(anyNumb);
+console.log(stringNumb);
+console.log(typeof stringNumb)
